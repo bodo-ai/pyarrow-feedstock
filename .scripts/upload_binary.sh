3890d13d95b165519b6e9d28e7526095e1a550ca
Srinivas Lade <srinulade1@gmail.com>
2024-07-29 15:35:13 -0400

    Upload to Bodo Anaconda Channel

--- .scripts/upload_binary.sh
+++ .scripts/upload_binary.sh
@@ -4,6 +4,7 @@ set -exo pipefail
 echo "********** Publishing to Artifactory **********"
 USERNAME=`cat /home/secrets/secret_file | grep artifactory.ci.username | cut -f 2 -d' '`
 TOKEN=`cat /home/secrets/secret_file | grep artifactory.ci.token | cut -f 2 -d' '`
+ANACONDA_TOKEN=`cat /home/secrets/secret_file | grep anaconda.org.token | cut -f 2 -d' '`
 
 CHANNEL_NAME="bodo.ai-platform"
 
@@ -12,6 +13,7 @@ for package in `ls ${FEEDSTOCK_ROOT}/build_artifacts/linux-64/*.conda`; do
     echo "Package Name: $package_name"
 
     curl -u${USERNAME}:${TOKEN} -T $package "https://bodo.jfrog.io/artifactory/${CHANNEL_NAME}/linux-64/$package_name"
+    anaconda -t $ANACONDA_TOKEN upload -u bodo.ai -c bodo.ai $package --label main --force
 done
 
 # Reindex Conda
